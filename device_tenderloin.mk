c5006cef73dd014027e054cd0d84dfb322645784
Wong Yong Jie <yjwong92@gmail.com>
2012-05-05 08:12:02 +0800

    fix changes

--- device_tenderloin.mk
+++ device_tenderloin.mk
@@ -1,6 +1,5 @@
 #
 # Copyright (C) 2011 The CyanogenMod Project
-# Copyright (C) 2012 The Classicnerd Project
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
@@ -15,43 +14,42 @@
 # limitations under the License.
 #
 
-# Proprietary stuff.
-$(call inherit-product-if-exists, vendor/hp/tenderloin/tenderloin-vendor.mk)
+## (1) First, the most specific values, i.e. the aspects that are specific to GSM
+PRODUCT_PROPERTY_OVERRIDES += \
+    ro.sf.lcd_density=160 \
+    ro.com.google.networklocation=1 \
+    dalvik.vm.lockprof.threshold=500 \
+    dalvik.vm.dexopt-flags=m=y
 
-# This is the hardware-specific overlay, which points to the location
-# of hardware-specific resource overrides, typically the frameworks and
-# application settings that are stored in resourced.
-DEVICE_PACKAGE_OVERLAYS += device/hp/tenderloin/overlay
+PRODUCT_CHARACTERISTICS := tablet
 
-# Custom init files.
+PRODUCT_AAPT_CONFIG := xlarge mdpi
+
+PRODUCT_PACKAGES += \
+	make_ext4fs
+
 PRODUCT_COPY_FILES += \
-    device/hp/tenderloin/init.tenderloin.rc:root/init.tenderloin.rc \
     device/hp/tenderloin/init.tenderloin.usb.rc:root/init.tenderloin.usb.rc \
-    device/hp/tenderloin/ueventd.tenderloin.rc:root/ueventd.tenderloin.rc
-
-PRODUCT_COPY_FILES += \
+    device/hp/tenderloin/init.tenderloin.rc:root/init.tenderloin.rc \
+    device/hp/tenderloin/ueventd.tenderloin.rc:root/ueventd.tenderloin.rc \
+    device/hp/tenderloin/initlogo.rle:root/initlogo.rle \
     device/hp/tenderloin/prebuilt/wifi/wpa_supplicant.conf:system/etc/wifi/wpa_supplicant.conf \
     device/hp/tenderloin/init.qcom.bt.sh:system/etc/init.qcom.bt.sh \
-    device/hp/tenderloin/prebuilt/bluetooth/hciattach:system/bin/hciattach_awesome \
-    device/hp/tenderloin/makemulti.sh:makemulti.sh \
-    device/hp/tenderloin/prebuilt/boot/moboot.splash.Classicnerd.tga:moboot.splash.Classicnerd.tga \
-    device/hp/tenderloin/prebuilt/boot/moboot.background.tga:moboot.background.tga \
-    device/hp/tenderloin/prebuilt/boot/moboot.default:moboot.default
-
-# Add touchcreen configuration file
-PRODUCT_COPY_FILES += \
     device/hp/tenderloin/HPTouchpad.idc:system/usr/idc/HPTouchpad.idc
 
 # Dualboot Magic
 PRODUCT_COPY_FILES += \
     device/hp/tenderloin/boot_webos:system/bin/boot_webos
 
-# Vold configuration
+# media minor check boot script
 PRODUCT_COPY_FILES += \
-    device/hp/tenderloin/vold.fstab:system/etc/vold.fstab \
+    device/hp/tenderloin/prebuilt/etc/init.d/10check_media_minor:system/etc/init.d/10check_media_minor
 
-PRODUCT_COPY_FILES += \
-    device/hp/tenderloin/media_profiles.xml:/system/etc/media_profiles.xml \
+## (2) Also get non-open-source GSM-specific aspects if available
+$(call inherit-product-if-exists, vendor/hp/tenderloin/tenderloin-vendor.mk)
+## (3)  Finally, the least specific parts, i.e. the non-GSM-specific aspects
+
+DEVICE_PACKAGE_OVERLAYS += device/hp/tenderloin/overlay
 
 PRODUCT_COPY_FILES += \
     frameworks/base/data/etc/android.hardware.telephony.gsm.xml:system/etc/permissions/android.hardware.telephony.gsm.xml \
@@ -68,7 +66,56 @@ PRODUCT_COPY_FILES += \
     frameworks/base/data/etc/android.hardware.sensor.gyroscope.xml:system/etc/permissions/android.hardware.sensor.gyroscope.xml \
     frameworks/base/data/etc/android.hardware.touchscreen.multitouch.distinct.xml:system/etc/permissions/android.hardware.touchscreen.multitouch.distinct.xml \
     frameworks/base/data/etc/android.hardware.usb.accessory.xml:system/etc/permissions/android.hardware.usb.accessory.xml \
+    frameworks/base/data/etc/android.hardware.usb.host.xml:system/etc/permissions/android.hardware.usb.host.xml \
     frameworks/base/data/etc/android.software.sip.voip.xml:system/etc/permissions/android.software.sip.voip.xml
+
+# QCOM Hal
+PRODUCT_PACKAGES += \
+    copybit.msm8660 \
+    hwcomposer.msm8660 \
+    gralloc.msm8660 \
+    liboverlay \
+    libmemalloc \
+    libtilerenderer \
+    libgenlock \
+    libQcomUI
+
+# QCOM OMX
+PRODUCT_PACKAGES += \
+	libstagefrighthw \
+	libOmxCore \
+	libmm-omxcore \
+	libdivxdrmdecrypt \
+	libOmxVdec
+
+# QCOM OMX Video Encoding/Tests
+#PRODUCT_PACKAGES += \
+#	mm-vdev-omx-test \
+#	mm-video-driver-test \
+#	libOmxVenc \
+#	mm-venc-omx-test720p \
+#	mm-video-encdrv-test
+
+# Audio
+PRODUCT_PACKAGES += \
+    audio.a2dp.default \
+    libaudioutils \
+    audio.primary.tenderloin \
+
+# Sensors, misc
+PRODUCT_PACKAGES += \
+    librs_jni \
+    wpa_supplicant.conf \
+    sensors.tenderloin \
+    lights.tenderloin \
+    ts_srv \
+    ts_srv_set \
+    dosfsck \
+    bcattach \
+    serial \
+    com.android.future.usb.accessory \
+    rebootcmd \
+    TenderloinParts
 
 # Keylayouts
 PRODUCT_COPY_FILES += \
@@ -80,23 +127,15 @@ PRODUCT_COPY_FILES += \
     device/hp/tenderloin/prebuilt/usr/keylayout/handset.kl:system/usr/keylayout/handset.kl \
     device/hp/tenderloin/prebuilt/usr/keylayout/gpio-keys.kl:system/usr/keylayout/gpio-keys.kl \
     device/hp/tenderloin/prebuilt/usr/keylayout/AVRCP.kl:system/usr/keylayout/AVRCP.kl \
-    device/hp/tenderloin/prebuilt/usr/keylayout/pmic8058_pwrkey.kl:system/usr/keylayout/pmic8058_pwrkey.kl
-
-# Kernel modules
+    device/hp/tenderloin/prebuilt/usr/keylayout/pmic8058_pwrkey.kl:system/usr/keylayout/pmic8058_pwrkey.kl \
+    device/hp/tenderloin/prebuilt/bluetooth/hciattach:system/bin/hciattach_awesome 
+    
+# Wifi Modules
 PRODUCT_COPY_FILES += \
-    device/hp/tenderloin/prebuilt/modules/ansi_cprng.ko:system/lib/modules/ansi_cprng.ko \
     device/hp/tenderloin/prebuilt/wifi/ar6000.ko:system/lib/modules/ar6000.ko \
     device/hp/tenderloin/prebuilt/modules/cifs.ko:system/lib/modules/cifs.ko \
-    device/hp/tenderloin/prebuilt/modules/cpaccess.ko:system/lib/modules/cpaccess.ko \
-    device/hp/tenderloin/prebuilt/modules/dma_test.ko:system/lib/modules/dma_test.ko \
-    device/hp/tenderloin/prebuilt/modules/gspca_main.ko:system/lib/modules/gspca_main.ko \
-    device/hp/tenderloin/prebuilt/modules/lcd.ko:system/lib/modules/lcd.ko \
-    device/hp/tenderloin/prebuilt/modules/librasdioif.ko:system/lib/modules/librasdioif.ko \
-    device/hp/tenderloin/prebuilt/modules/nls_utf8.ko:system/lib/modules/nls_utf8.ko \
     device/hp/tenderloin/prebuilt/modules/ntfs.ko:system/lib/modules/ntfs.ko \
-    device/hp/tenderloin/prebuilt/modules/oprofile.ko:system/lib/modules/oprofile.ko \
-    device/hp/tenderloin/prebuilt/modules/scsi_wait_scan.ko:system/lib/modules/scsi_wait_scan.ko \
-    device/hp/tenderloin/prebuilt/modules/tcp_bic.ko:system/lib/modules/tcp_bic.ko \
+    device/hp/tenderloin/prebuilt/modules/nls_utf8.ko:system/lib/modules/nls_utf8.ko \
     device/hp/tenderloin/prebuilt/modules/tun.ko:system/lib/modules/tun.ko
 
 #Wifi Firmware
@@ -115,6 +154,7 @@ PRODUCT_COPY_FILES += \
     device/hp/tenderloin/prebuilt/wifi/ath6k/AR6003/hw2.1.1/athwlan.bin:/system/etc/firmware/ath6k/AR6003/hw2.1.1/athwlan.bin \
     device/hp/tenderloin/prebuilt/wifi/ath6k/AR6003/hw2.1.1/bdata.SD32.bin:/system/etc/firmware/ath6k/AR6003/hw2.1.1/bdata.SD32.bin \
     device/hp/tenderloin/prebuilt/wifi/ath6k/AR6003/hw2.1.1/otp.bin:/system/etc/firmware/ath6k/AR6003/hw2.1.1/otp.bin \
+    device/hp/tenderloin/prebuilt/wifi/ath6k/AR6003/hw2.1.1/fw-3.bin:/system/etc/firmware/ath6k/AR6003/hw2.1.1/fw-3.bin \
     device/hp/tenderloin/prebuilt/wifi/ath6k/AR6003/hw1.0/data.patch.bin:/system/etc/firmware/ath6k/AR6003/hw1.0/data.patch.bin \
     device/hp/tenderloin/prebuilt/wifi/ath6k/AR6003/hw1.0/bdata.SD31.bin:/system/etc/firmware/ath6k/AR6003/hw1.0/bdata.SD31.bin \
     device/hp/tenderloin/prebuilt/wifi/ath6k/AR6003/hw1.0/athwlan.bin.z77:/system/etc/firmware/ath6k/AR6003/hw1.0/athwlan.bin.z77 \
@@ -130,81 +170,17 @@ PRODUCT_COPY_FILES += \
     device/hp/tenderloin/prebuilt/wifi/ath6k/AR6003/hw2.1.1/bdata.SD32.bin:/system/etc/firmware/ath6k/AR6003/hw2.1.1/bdata.CUSTOM.bin \
     device/hp/tenderloin/prebuilt/wifi/ath6k/AR6003/hw1.0/bdata.SD32.bin:/system/etc/firmware/ath6k/AR6003/hw1.0/bdata.CUSTOM.bin
 
-# We have enough storage space to hold precise GC data
+# we have enough storage space to hold precise GC data
 PRODUCT_TAGS += dalvik.gc.type-precise
 
-# This is a tablet.
-PRODUCT_CHARACTERISTICS := tablet
-PRODUCT_AAPT_CONFIG := xlarge mdpi
-PRODUCT_AAPT_PREF_CONFIG := mdpi
+# device uses high-density artwork where available
+PRODUCT_LOCALES += en_US mdpi
 
-# QCOM Hal
-PRODUCT_PACKAGES += \
-    copybit.msm8660 \
-    hwcomposer.msm8660 \
-    gralloc.msm8660 \
-    liboverlay \
-    libmemalloc \
-    libtilerenderer \
-    libgenlock \
-    libQcomUI
-
-# QCOM OMX
-PRODUCT_PACKAGES += \
-    libstagefrighthw \
-    libOmxCore \
-    libmm-omxcore \
-    libdivxdrmdecrypt \
-    libOmxVdec
-
-# QCOM OMX Video Encoding/Tests
-#PRODUCT_PACKAGES += \
-#   mm-vdev-omx-test \
-#   mm-video-driver-test \
-#   libOmxVenc \
-#   mm-venc-omx-test720p \
-#   mm-video-encdrv-test
-
-# Audio
-PRODUCT_PACKAGES += \
-    audio.a2dp.default \
-    libaudioutils \
-    audio.primary.tenderloin \
-
-# Sensors, misc
-PRODUCT_PACKAGES += \
-    librs_jni \
-    wpa_supplicant.conf \
-    sensors.tenderloin \
-    lights.tenderloin \
-    ts_srv \
-    dosfsck \
-    bcattach \
-    serial \
-    com.android.future.usb.accessory \
-    rebootcmd
-
-PRODUCT_PACKAGES += \
-	make_ext4fs
-
-# Property overrides.
-PRODUCT_PROPERTY_OVERRIDES += \
-    ro.sf.lcd_density=160 \
-    ro.com.google.networklocation=1 \
-    dalvik.vm.lockprof.threshold=500 \
-    dalvik.vm.dexopt-flags=m=y
-
-# Prebuilt kernel.
-ifeq ($(TARGET_PREBUILT_KERNEL),)
-	LOCAL_KERNEL := device/hp/tenderloin/prebuilt/boot/kernel
-else
-	LOCAL_KERNEL := $(TARGET_PREBUILT_KERNEL)
-endif
-
-# Copy the kernel.
 PRODUCT_COPY_FILES += \
-    $(LOCAL_KERNEL):kernel
+    device/hp/tenderloin/media_profiles.xml:/system/etc/media_profiles.xml \
+    device/hp/tenderloin/vold.fstab:system/etc/vold.fstab \
+    device/hp/tenderloin/makemulti.sh:makemulti.sh \
+    device/hp/tenderloin/prebuilt/boot/moboot.splash.CyanogenMod.tga:moboot.splash.CyanogenMod.tga
 
-$(call inherit-product, $(SRC_TARGET_DIR)/product/full_base.mk)
 $(call inherit-product, frameworks/base/build/tablet-dalvik-heap.mk)
-$(call inherit-product, $(SRC_TARGET_DIR)/product/languages_full.mk)
+$(call inherit-product, build/target/product/full_base.mk)
